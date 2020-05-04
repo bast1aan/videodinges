2b5785444f3eb3efca9222f59546925a00e53907
Bastiaan Welmers <bastiaan@welmers.net>
2020-05-04 21:36:09 +0200

    Added better upload model

--- videodinges/admin.py
+++ videodinges/admin.py
@@ -1,10 +1,26 @@
+from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 
 from . import models
 
-class TranscodingsInline(admin.TabularInline):
+class TranscodingsForm(forms.ModelForm):
+	def clean(self):
+		cleaned_data = super().clean()
+		if not cleaned_data['url'] and not cleaned_data['upload']:
+			validation_error = ValidationError('Either url or upload must be given', code='invalid')
+			self.add_error('url', validation_error)
+			self.add_error('upload', validation_error)
+		if cleaned_data['url'] and cleaned_data['upload']:
+			validation_error = ValidationError('Cannot fill both url and upload', code='invalid')
+			self.add_error('url', validation_error)
+			self.add_error('upload', validation_error)
+		return cleaned_data
+
+class TranscodingsInline(admin.StackedInline):
 	model = models.Transcoding
-	fields = ['quality', 'file']
+	form = TranscodingsForm
+	fields = ['quality', 'url', 'upload']
 	extra = 0
 
 class VideoAdmin(admin.ModelAdmin):
@@ -13,3 +29,4 @@ class VideoAdmin(admin.ModelAdmin):
 	inlines = [TranscodingsInline]
 
 admin.site.register(models.Video, VideoAdmin)
+admin.site.register(models.Upload)
