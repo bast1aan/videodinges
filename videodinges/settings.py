7daff47d5c5698c3e48bc380e61f7adc62f053e0
Bastiaan Welmers <bastiaan@welmers.net>
2020-05-05 14:25:03 +0200

    No prefixing slash in URLs

--- videodinges/settings.py
+++ videodinges/settings.py
@@ -124,13 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
-MEDIA_URL = '/uploads/'
+MEDIA_URL = 'uploads/'
 
 FILE_UPLOAD_HANDLERS = ['django.core.files.uploadhandler.TemporaryFileUploadHandler']
 
