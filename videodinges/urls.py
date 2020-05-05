4bff78d0ac0f06ef509726c384d0ca25d83f2d5c
Bastiaan Welmers <bastiaan@welmers.net>
2020-05-05 12:36:23 +0200

    Fix uploads serving

--- videodinges/urls.py
+++ videodinges/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -22,6 +24,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^(?P<slug>[\w-]+).html', views.video)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 for i in testviews.__all__:
 	urlpatterns.append(url(r'^test/{}$'.format(i), testviews.__dict__[i]))
